c6df9fa08374f77531f9320fb141d9aefb093cee
andre-goldmann <andre_goldmann@yahoo.de>
2024-09-29 19:13:34 +0200

    hide me

---
+++ mynode.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+
+const envFile = `import { AppConfig } from '../app/app-config';
+export const environment: AppConfig = {
+    apiUrl: ${process.env.production},
+    projectId: '${process.env.baseURL}'
+};
+`;
+const targetPath = path.join(__dirname, './src/environments/environment.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+  }
+});
--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,7 +1,6 @@
 import { Client, Account} from 'appwrite';
 import { environment } from '../environments/environment';
 
-console.log(environment.apiUrl);
 const client = new Client();
 
 client
