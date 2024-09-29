54399a53cb297cc41b3a34377544fdafdcd45225
andre-goldmann <andre_goldmann@yahoo.de>
2024-09-29 08:33:18 +0200

    mapp added

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,14 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent  {
+
+
+
+}
