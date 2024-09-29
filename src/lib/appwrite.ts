45982f4f7aec4ad3aec48b2e5837296f6530d9df
andre-goldmann <andre_goldmann@yahoo.de>
2024-09-29 19:04:33 +0200

    hide me

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,10 +1,12 @@
 import { Client, Account} from 'appwrite';
+import { environment } from '../environments/environment';
 
+console.log(environment.apiUrl);
 const client = new Client();
 
 client
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('66f822a90035ffda3208');
+  .setEndpoint(environment.apiUrl)
+  .setProject(environment.projectId);
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
